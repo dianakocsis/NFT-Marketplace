5c3f919a514565199607cf224e1e02b7089dd9ec
Diana Kocsis <dlk61@pitt.edu>
2024-01-16 14:37:20 -0500

    fix address

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -9,18 +9,20 @@ import {
 } from '../typechain-types';
 
 async function main() {
-  const [owner, platformFeeRecipient, royaltyReceiver] =
-    await ethers.getSigners();
-
+  const [owner] = await ethers.getSigners();
+  const [platformFeeRecipient] = await ethers.getSigners();
+  const [royaltyReceiver] = await ethers.getSigners();
   const Marketplace = (await ethers.getContractFactory(
     'Marketplace'
   )) as Marketplace__factory;
   const marketplace = (await Marketplace.deploy(
-    owner,
+    owner.address,
     1000,
-    platformFeeRecipient
+    platformFeeRecipient.address
   )) as Marketplace;
   await marketplace.waitForDeployment();
+
+  console.log(`marketplace deployed to ${marketplace.target}`);
 
   const ERC20Test = (await ethers.getContractFactory(
     'ERC20Test'
@@ -28,22 +30,26 @@ async function main() {
   const erc20Test = (await ERC20Test.deploy('ERC20Test', 'FT')) as ERC20Test;
   await erc20Test.waitForDeployment();
 
+  console.log(`erc20test deployed to ${erc20Test.target}`);
+
   const ERC721Test = (await ethers.getContractFactory(
     'ERC721Test'
   )) as ERC721Test__factory;
   const erc721Test = (await ERC721Test.deploy(
     'ERC721Test',
     'NFT',
-    royaltyReceiver,
+    royaltyReceiver.address,
     1000
   )) as ERC721Test;
   await erc721Test.waitForDeployment();
+
+  console.log(`erc721test deployed to ${erc721Test.target}`);
 
   await erc721Test.deploymentTransaction()?.wait(5);
 
   await hre.run('verify:verify', {
     address: marketplace.target,
-    constructorArguments: [owner, 1000, platformFeeRecipient],
+    constructorArguments: [owner.address, 1000, platformFeeRecipient.address],
   });
 
   await hre.run('verify:verify', {
@@ -53,7 +59,7 @@ async function main() {
 
   await hre.run('verify:verify', {
     address: erc721Test.target,
-    constructorArguments: ['ERC721Test', 'NFT', royaltyReceiver, 1000],
+    constructorArguments: ['ERC721Test', 'NFT', royaltyReceiver.address, 1000],
   });
 }
 
